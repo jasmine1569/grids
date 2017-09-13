675ef95d4960af7f82889784384f5e541cd46a8e
Jasmine Beeman <jasmine.beeman@metalab.csun.edu>
2017-09-13 14:51:00 -0700

    added file and modified:

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 // Everything Is Ready
 console.log('Grids System');
+
+// (function() {
+//     var path = 'https://easy.myfonts.net/v2/js?sid=230539(font-family=Brandon+Text+Bold)&sid=230543(font-family=Brandon+Text+Light)&sid=230545(font-family=Brandon+Text+Medium)&sid=230546(font-family=Brandon+Text+Regular)&key=Sr6C9xVhlL',
+//         protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+//         trial = document.createElement('script');
+//     trial.type = 'text/javascript';
+//     trial.async = true;
+//     trial.src = protocol + path;
+//     var head = document.getElementsByTagName("head")[0];
+//     head.appendChild(trial);
+// })();
