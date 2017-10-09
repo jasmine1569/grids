eecef3979ba6314db38d6041ea727fa004e6f20d
Jasmine Beeman <jasmine.beeman@metalab.csun.edu>
2017-10-09 10:02:19 -0700

    yarn run dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ const mix = require('laravel-mix');
 var bowerPath = './node_modules';
 
 // mix.copy(`${bowerPath}/font-awesome/scss`, 'src/sass/vendor/font-awesome');
-// mix.copy(`${bowerPath}/font-awesome/fonts`, 'dist/fonts');
+mix.copy(`${bowerPath}/font-awesome/fonts`, 'dist/fonts');
 
 mix.js('src/js/app.js', 'dist/js')
   .sass('src/sass/app.scss', 'dist/css');
